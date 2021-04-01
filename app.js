fe665693dd5a4eec19064dfd5fbcc2ef66466cf9
Ashutosh Raj <ashutoshraj908@gmail.com>
2021-04-02 01:06:33 +0530

    Game over functionality added

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ const app = Vue.createApp({
         return{
             playerHealth: 100,
             monsterHealth: 100, 
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         };
+    },
+    watch: {
+        playerHealth(value){
+            if(value <= 0 && this.monsterHealth <= 0){
+               // Draw 
+               this.winner = 'draw';
+            }else if(value <= 0){ 
+                // player lost
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value){
+            if(value <= 0 && this.monsterHealth <= 0){
+               // Draw 
+               this.winner = 'draw';
+            }else if(value <= 0){ 
+                // monster lost
+                this.winner = 'player';
+            }
+        }
     },
     computed: {
         monsterBarStyles(){
